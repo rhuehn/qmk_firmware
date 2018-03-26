9152b5f14e3c3dbdd8c07906d5666f729f7333fb
Rob Huehn <rhuehn@gmail.com>
2018-03-26 00:37:39 -0700

    dz60/keymaps/rhuehn: real aliases

--- keyboards/dz60/keymaps/rhuehn/keymap.c
+++ keyboards/dz60/keymaps/rhuehn/keymap.c
@@ -5,12 +5,6 @@
 #define ___X___ KC_NO
 
 #define MO_ESC LT(MO(1),KC_ESC)
-#define KP_MINU KC_KP_MINUS
-#define KP_PLUS KC_KP_PLUS
-#define KP_SLAS KC_KP_SLASH
-#define KP_ASTE KC_KP_ASTERISK
-#define KP_ENTE KC_KP_ENTER
-#define KP_DOT KC_KP_DOT
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KEYMAP(
@@ -21,17 +15,17 @@ KEYMAP(
 	KC_LCTL,          KC_LGUI, KC_LALT, KC_BSPC,          MO(1)  ,          KC_SPC , KC_RALT, KC_RGUI, ___X___, MO(2)  ,          KC_RCTL),
 
 KEYMAP(
-	_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL ,
+	_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , ___X___, KC_DEL ,
 	_______,          _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, KC_PGUP, KC_HOME, KC_UP  , KC_END , _______, _______, _______, _______,
 	KC_CAPS,          _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          _______,
-	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-	_______,          _______, _______, KC_DEL ,          _______,          KC_INS , _______, _______, _______, _______,          _______),
+	_______, ___X___, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          ___X___,
+	_______,          _______, _______, KC_DEL ,          _______,          KC_INS , _______, _______, ___X___, _______,          _______),
 
 KEYMAP(
-	_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR, KC_SLCK, KC_PAUSE,_______, _______, _______, _______, RESET  , 
-	_______, 	  RGB_TOG, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KP_MINU, KP_SLAS, _______, _______, 
-	_______, 	  _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KP_PLUS, KP_ASTE, _______, 
-	_______, _______, BL_TOGG, _______, _______, _______, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KP_ENTE, _______,	      _______, 
-	_______, 	  _______, _______, _______,          _______,          KP_DOT , _______, _______, _______, _______,	      _______),
+	_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, ___X___, RESET  , 
+	_______, 	  RGB_TOG, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_PSLS, _______, _______, 
+	_______, 	  _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_PAST,          KC_PENT, 
+	_______, ___X___, BL_TOGG, _______, _______, _______, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_PENT, _______,	      ___X___, 
+	_______, 	  _______, _______, _______,          _______,          KC_PDOT, _______, _______, ___X___, _______,	      _______),
 };
 
