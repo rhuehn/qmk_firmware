dc38737bdda85a2ec61b2f29e93204c4b8c4380f
Rob Huehn <rhuehn@gmail.com>
2018-06-23 15:18:17 -0700

    dz60/keymaps/rhuehn: add Colemak DpgHm layer

--- keyboards/dz60/keymaps/rhuehn/keymap.c
+++ keyboards/dz60/keymaps/rhuehn/keymap.c
@@ -1,6 +1,6 @@
 #include "dz60.h"
 
-#define FN1 3
+#define FN1 4
 #define FN2 (FN1 + 1)
 
 #define _______ KC_TRNS
@@ -26,7 +26,15 @@ KEYMAP(
     KC_LSFT, ___X___, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          ___X___,
     KC_LCTL,          KC_LGUI, KC_LALT, KC_BSPC,          FN1_ESC,          KC_SPC , MO(FN2), KC_RALT, ___X___, KC_APP ,          KC_RCTL),
 
-// 2 Colemak Mod-DH ANSI https://colemakmods.github.io/mod-dh/gfx/mod_dh_keyb_ansi.png
+// 2 Colemak DpgHm http://colemakmods.github.io/ergonomic-mods/gfx/curl_dpghm_ansi.png
+KEYMAP(
+    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , ___X___, KC_DEL ,
+    KC_TAB ,          KC_Q   , KC_W   , KC_F   , KC_D   , KC_P   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
+    FN1_ESC,          KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , KC_O,    KC_QUOT,          KC_ENT ,
+    KC_LSFT, ___X___, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          ___X___,
+    KC_LCTL,          KC_LGUI, KC_LALT, KC_BSPC,          FN1_ESC,          KC_SPC , MO(FN2), KC_RALT, ___X___, KC_APP ,          KC_RCTL),
+
+// 3 Colemak Mod-DH ANSI https://colemakmods.github.io/mod-dh/gfx/mod_dh_keyb_ansi.png
 KEYMAP(
     KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , ___X___, KC_DEL ,
     KC_TAB ,          KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
@@ -34,7 +42,7 @@ KEYMAP(
     KC_LSFT, ___X___, KC_X   , KC_C   , KC_D   , KC_V   , KC_Z   , KC_M   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          ___X___,
     KC_LCTL,          KC_LGUI, KC_LALT, KC_BSPC,          FN1_ESC,          KC_SPC , MO(FN2), KC_RALT, ___X___, KC_APP ,          KC_RCTL),
 
-// 3 FN1
+// 4 FN1
 KEYMAP(
     ___X___, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , ___X___, KC_INS ,
     ___X___,          ___X___, KC_HOME, KC_UP  , KC_END , KC_PGUP, KC_PGUP, KC_HOME, KC_UP  , KC_END , ___X___, ___X___, ___X___, ___X___,
@@ -42,12 +50,12 @@ KEYMAP(
     _______, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, _______,          ___X___,
     _______,          _______, _______, KC_DEL ,          _______,          SFT_SPC, TG(FN2), KC_RGUI, ___X___, _______,          _______),
 
-// 4 FN2
+// 5 FN2
 KEYMAP(
     ___X___, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR, KC_SLCK, KC_PAUS, ___X___, ___X___, ___X___, ___X___, RESET  , 
     _______,          RGB_TOG, ___X___, ___X___, ___X___, ___X___, ___X___, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_PSLS, ___X___, ___X___, 
     _______,          ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_PAST,          KC_PENT, 
-    _______, ___X___, DF(0),   DF(1),   DF(2),   ___X___, ___X___, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_PDOT, _______,          ___X___, 
+    _______, ___X___, DF(0),   DF(1),   DF(2),   DF(3),   ___X___, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, KC_PDOT, _______,          ___X___, 
     _______,          _______, _______, _______,          _______,          _______, _______, _______, ___X___, _______,          _______),
 };
 
